08b581f86a358fa7fded9c72370d619d661e2673
furude.K <hurudestars@gmail.com>
2021-07-07 15:27:57 +0900

    UserListControllerを更新

--- src/main/java/com/example/controller/UserListController.java
+++ src/main/java/com/example/controller/UserListController.java
@@ -1,15 +1,34 @@
 package com.example.controller;
+
+import com.example.domain.user.model.MUser;
+import com.example.domain.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.LinkedHashMap;
+import java.util.List;
 
 @Controller
 @RequestMapping("/user")
 public class UserListController {
 
-    /** ユーザー一覧画面を表示 */
+    @Autowired
+    private UserService userService;
+
+    /**
+     * ユーザー一覧画面を表示
+     */
     @GetMapping("/list")
-    public String getUserList() {
+    public String getUserList(Model model) {
+        //ユーザー一覧を取得
+        List<MUser> userList = userService.getUsers();
+
+        //Modelに登録
+        model.addAttribute("userList", userList);
+
         // ユーザー一覧画面を表示
         return "user/list";
     }
