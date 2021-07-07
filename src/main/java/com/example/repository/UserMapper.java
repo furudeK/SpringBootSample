8328e2f80996bfecd9c2ff05804d9ecd8fb8d573
furude.K <hurudestars@gmail.com>
2021-07-07 15:19:33 +0900

    UserMapper.javaを更新

--- src/main/java/com/example/repository/UserMapper.java
+++ src/main/java/com/example/repository/UserMapper.java
@@ -3,8 +3,13 @@ package com.example.repository;
 import org.apache.ibatis.annotations.Mapper;
 import com.example.domain.user.model.MUser;
 
+import java.util.List;
+
 @Mapper
 public interface UserMapper {
     /** ユーザー登録 */
     public int insertOne(MUser user);
+
+    /** ユーザー取得 */
+    public List<MUser> findMany();
 }
