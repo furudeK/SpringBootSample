71efe209c244ad4eaa5852a93f63925f47696cd4
furude.K <hurudestars@gmail.com>
2021-07-15 12:24:32 +0900

    11.2.6 done

--- src/main/java/com/example/controller/LogoutController.java
+++ src/main/java/com/example/controller/LogoutController.java
@@ -4,13 +4,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 
+/** 247pから不要となった。 */
 @Controller
 @Slf4j
 public class LogoutController {
-    /** ログイン画面にリダイレクト */
-    @PostMapping("/logout")
-    public String postLogout() {
-        log.info("ログアウト");
-        return "redirect:/login";
-    }
+//    /** ログイン画面にリダイレクト */
+//    @PostMapping("/logout")
+//    public String postLogout() {
+//        log.info("ログアウト");
+//        return "redirect:/login";
+//    }
 }
