dbcf677f1347c3f0d4265878ddf5780b3858e796
furude.K <hurudestars@gmail.com>
2021-07-15 11:36:49 +0900

    11.2.3 done

--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -49,5 +50,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         // CSRF対策を無効に設定（一時的）
         http.csrf().disable();
+    }
+
+    /**
+     * 認証の設定
+     */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //インメモリ認証
+        auth
+                .inMemoryAuthentication()
+                .withUser("user")//userを追加
+                .password("user")
+                .roles("GENERAL")
+                .and()
+                .withUser("admin")//admin を追加
+                .password("admin")
+                .roles("ADMIN");
     }
 }
