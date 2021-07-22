1e6cfe0d60b0e2d8e86ff61823e7a84a205a378f
furude.K <hurudestars@gmail.com>
2021-07-22 16:25:19 +0900

    update code for 13.3.2

--- src/main/java/com/example/domain/user/model/MUser.java
+++ src/main/java/com/example/domain/user/model/MUser.java
@@ -4,10 +4,8 @@ import java.util.Date;
 import java.util.List;
 
 import lombok.Data;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.Transient;
+
+import javax.persistence.*;
 
 @Data
 @Entity
@@ -22,7 +20,10 @@ public class MUser {
     private Integer gender;
     private Integer departmentId;
     private String role;
-    @Transient
+
+
+    @ManyToOne(optional = true)
+    @JoinColumn(insertable = false, updatable = false, name = "departmentId")
     private Department department;
     @Transient
     private List<Salary> salaryList;
